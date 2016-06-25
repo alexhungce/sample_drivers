a22072760b53b3650a900681fffa0944bddf0621
Alex Hung <alex.hung@canonical.com>
2016-06-25 16:29:26 +0800

    add an empty driver
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

---
+++ sample/sample.c
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (C) 2016 Alex Hung <alex.hung@canonical.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alex Hung");
+
+static int __init sample_init(void)
+{
+
+	return 0;
+}
+
+static void __exit sample_exit(void)
+{
+
+}
+
+module_init(sample_init);
+module_exit(sample_exit);
